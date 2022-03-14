2e6090012ae09ed2e70e0bbb8c86737f902577f5
Younggun Park <ygpark0922@gmail.com>
2022-03-14 17:54:35 +0900

    feat: user interface change to support reactive design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,200 @@
 import * as React from 'react';
-import { Box, Drawer, CssBaseline, AppBar, Toolbar, List, Typography, Divider, ListItem, ListItemIcon, ListItemText } from '@mui/material';
-import { MoveToInbox, Mail } from '@mui/icons-material';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 import SeatTable from './pages/contents/SeatTable';
 import MeetingRoomReservation from "./pages/contents/MeetingRoomReservation";
+import IPTable from "./pages/contents/IPTable";
+import Board from "./pages/contents/Board";
+import {Link, Route, Routes} from "react-router-dom";
+import {ListItem} from "@mui/material";
 
 const drawerWidth = 240;
 
-function App() {
-  return (
-      <Box className="App" sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <AppBar
-            position="fixed"
-            sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-        >
-          <Toolbar>
-            <Typography variant="h6" noWrap component="div">
-              E105
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-            sx={{
-              width: drawerWidth,
-              flexShrink: 0,
-              '& .MuiDrawer-paper': {
-                width: drawerWidth,
-                boxSizing: 'border-box',
-              },
-            }}
-            variant="permanent"
-            anchor="left"
-        >
-          <Toolbar />
-          <Divider />
-          <List>
-            {['게시판', '미팅 테이블 예약', '자리 배정', 'IP 테이블'].map((text, index) => (
-                <ListItem button key={text}>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <MoveToInbox /> : <Mail />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItem>
-            ))}
-          </List>
-        </Drawer>
-        <Box
-            component="main"
-            sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
-        >
-          <Toolbar />
-            <MeetingRoomReservation />
-        </Box>
-      </Box>
-  );
-}
+const openedMixin = (theme) => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+});
 
-export default App;
+const closedMixin = (theme) => ({
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        ...(open && {
+            ...openedMixin(theme),
+            '& .MuiDrawer-paper': openedMixin(theme),
+        }),
+        ...(!open && {
+            ...closedMixin(theme),
+            '& .MuiDrawer-paper': closedMixin(theme),
+        }),
+    }),
+);
+
+export default function App() {
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    function nextPath(path) {
+        this.props.history.push(path);
+    }
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position="fixed" open={open}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{
+                            marginRight: 5,
+                            ...(open && { display: 'none' }),
+                        }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        Mini variant drawer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer variant="permanent" open={open}>
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                    </IconButton>
+                </DrawerHeader>
+                <Divider />
+                <List>
+                    {['게시판', '미팅 테이블 예약', '자리 배정', 'IP 테이블'].map((text, index) => (
+                            <ListItemButton
+                            key={text}
+                            sx={{
+                                minHeight: 48,
+                                justifyContent: open ? 'initial' : 'center',
+                                px: 2.5,
+                            }}
+                            to={'/' + index}
+                            >
+                            <ListItemIcon
+                                sx={{
+                                    minWidth: 0,
+                                    mr: open ? 3 : 'auto',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+                        </ListItemButton>
+                    ))}
+                </List>
+                <Divider />
+                <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                        <ListItemButton
+                            key={text}
+                            sx={{
+                                minHeight: 48,
+                                justifyContent: open ? 'initial' : 'center',
+                                px: 2.5,
+                            }}
+                        >
+                            <ListItemIcon
+                                sx={{
+                                    minWidth: 0,
+                                    mr: open ? 3 : 'auto',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+                        </ListItemButton>
+                    ))}
+                </List>
+            </Drawer>
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                <DrawerHeader />
+                <Routes>
+                    <Route exact path="/" component={Board} />
+                    <Route path="/0" component={Board} />
+                    <Route path="/1" component={MeetingRoomReservation} />
+                    <Route path="/2" component={SeatTable} />
+                    <Route path="/3" component={IPTable} />
+                </Routes>
+            </Box>
+        </Box>
+    );
+}
--- frontend/src/pages/contents/MeetingRoomReservation.js
+++ frontend/src/pages/contents/MeetingRoomReservation.js
@@ -16,23 +16,23 @@ import axios from "axios";
 export default function MeetingRoomReservation(){
     const [starttime, setstarttime] = React.useState(new Date('2022-01-01T00:00:00.000Z'));
     const [endtime, setendtime] = React.useState(new Date('2022-01-01T00:00:00.000Z'));
-    const [title, settitle] = React.useState('');
-    const [group, setgroup] = React.useState('');
+    const [title, settitle] = React.useState('ㅇㅇ`');
+    const [group, setgroup] = React.useState('메가브레인');
     const [num, setnum] = React.useState(1);
     return (
         <>
-            <Typography typography="h2">세미나실 예약 시스템</Typography>
+            <Typography typography="h2">미팅 테이블 예약 시스템</Typography>
             <br/>
                 <FormLabel id="demo-radio-buttons-group-label">사용목적</FormLabel>
-                <TextField id="outlined-basic" variant="outlined" onChange={(value)=>settitle(value)}/>
-                <br/>
+                <TextField id="outlined-basic" variant="outlined" onChange={(event)=>settitle(event.target.value)}/>
+                <br/>`
                 <FormLabel id="demo-radio-buttons-group-label">동아리 선택</FormLabel>
                 <RadioGroup
                     row
                     aria-labelledby="demo-radio-buttons-group-label"
-                    defaultValue="female"
+                    defaultValue="mega"
                     name="radio-buttons-group"
-                    onSelect={(value)=>setgroup(value)}
+                    onSelect={(event)=>setgroup(event.target.value)}
                 >
                     <FormControlLabel value="mega" control={<Radio />} label="메가브레인" />
                     <FormControlLabel value="dot" control={<Radio />} label="돗가비" />
@@ -44,7 +44,7 @@ export default function MeetingRoomReservation(){
                     id="standard-number"
                     label="Number"
                     type="number"
-                    onChange={(value)=>setnum(value)}
+                    onChange={(event)=>setnum(Number.valueOf(event.target.value))}
                     InputLabelProps={{
                         shrink: true,
                     }}
@@ -60,7 +60,6 @@ export default function MeetingRoomReservation(){
                             setstarttime(newValue);
                             console.log(starttime);
                         }}/>
-                    <br/>
                     <DateTimePicker
                         renderInput={(props) => <TextField {...props} />}
                         value={endtime}
@@ -73,22 +72,23 @@ export default function MeetingRoomReservation(){
                     />
                 </LocalizationProvider>
                 <Button onClick={()=>{
-                    axios({
-                        headers: {'Access-Control-Allow-Origin': '*'},
-                        method: "get",
-                        url: 'http://192.168.0.5:8080/api/v1/meeting',
-                        data: {
-                            purpose: title,
-                            team: group,
-                            usingPeople: Number.valueOf(num),
-                            starTime: starttime,
-                            endTime: endtime,
-                        }
+                    const data = {
+                        purpose: title,
+                        team: group,
+                        usingPeople: num,
+                        starTime: starttime.toISOString(),
+                        endTime: endtime.toISOString(),
+                    };
+                    axios.post('http://203.241.228.70:18080/api/v1/meeting', data, {
+                        headers: {
+                            'Access-Control-Allow-Origin': '*',
+                        },
                     })
                         .then((Response)=>{
                             console.log(Response.data)
+                            console.log(data)
                         })
-                        .catch((Error)=>{console.log(Error)})
+                        .catch((Error)=>{console.log(Error); console.log(data);})
                 }}>예약하기</Button>
         </>
     )
