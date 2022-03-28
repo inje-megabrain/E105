b7572b70b79fafe318102282d67a346593b31e43
sbs1621 <qudtjr4218@gmail.com>
2022-03-28 15:36:45 +0900

    페이지네이션 Sorting 구현

--- backend/src/main/java/org/megabrain/kimchijjige/controller/PageController.java
+++ backend/src/main/java/org/megabrain/kimchijjige/controller/PageController.java
@@ -2,8 +2,11 @@ package org.megabrain.kimchijjige.controller;
 
 import org.megabrain.kimchijjige.entity.Bord;
 import org.megabrain.kimchijjige.repository.BordRepository;
+import org.megabrain.kimchijjige.service.PageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,28 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class PageController {
     private BordRepository bordRepository;
+    @Autowired
+    private PageService pageService;
     public PageController(BordRepository repository){
         this.bordRepository = repository;
     }
 
     @GetMapping("/list")
-    public Page<Bord> getAllBord(@RequestParam("page") Integer page, @RequestParam("size") Integer size) {
-        PageRequest pageRequest = PageRequest.of(page, size );
-        return bordRepository.findAll(pageRequest);
+    public Page<Bord> getAllBord(@RequestParam("page") Integer page, @RequestParam("size") Integer size, @RequestParam("sortBy") String sortBY, @RequestParam("isAsc") Boolean isAsc) {
+
+        return pageService.getAllBord(page, size, sortBY ,isAsc);
     }
 
-    /* 보드 100개 생성
-    @PostConstruct
-    public void initializing(){
-        for (int i = 0; i < 100; i++){
-            Bord bord = Bord.builder()
-                    .author("User" + i)
-                    .content("Body")
-                    .createdTime(LocalDateTime.now())
-                    .modifiedTime(LocalDateTime.now())
-                    .title("title")
-                    .build();
-            bordRepository.save(bord);
-        }
-    }*/
 }
