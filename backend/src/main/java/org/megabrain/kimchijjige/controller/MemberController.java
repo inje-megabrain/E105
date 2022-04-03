2452347944470047a18ba84497063c74137b54b9
sbs1621 <qudtjr4218@gmail.com>
2022-04-03 16:43:36 +0900

    로그인 Session등록/삭제 구현

--- backend/src/main/java/org/megabrain/kimchijjige/controller/MemberController.java
+++ backend/src/main/java/org/megabrain/kimchijjige/controller/MemberController.java
@@ -2,6 +2,7 @@ package org.megabrain.kimchijjige.controller;
 
 import org.hibernate.JDBCException;
 import org.hibernate.exception.ConstraintViolationException;
+import org.megabrain.kimchijjige.auth.SessionManager;
 import org.megabrain.kimchijjige.dto.LoginDto;
 import org.megabrain.kimchijjige.dto.NewMemberDto;
 import org.megabrain.kimchijjige.entity.Member;
@@ -10,12 +11,16 @@ import org.megabrain.kimchijjige.repository.MemberRepository;
 import org.megabrain.kimchijjige.service.MemberService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
@@ -29,6 +34,8 @@ public class MemberController {
 
     private final MemberService memberService;
     private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+    @Autowired
+    private SessionManager sessionManager;
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
@@ -47,7 +54,8 @@ public class MemberController {
         return new ResponseEntity("회원가입 완료", HttpStatus.OK);
     }
 
-    @PostMapping("/login")
+
+     @PostMapping("/login")
     public @ResponseBody
     ResponseEntity login(@RequestBody LoginDto loginDto, HttpServletResponse response) {
         String login;
@@ -56,15 +64,18 @@ public class MemberController {
             login = memberService.login(loginDto);
             cookie = new Cookie("memberId", loginDto.getEmail());
             response.addCookie(cookie);
+
         } catch (RuntimeException e) {
             return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
+        sessionManager.createSession(loginDto.getEmail(), response);//세션등록 추가
         return new ResponseEntity(login, HttpStatus.OK);
     }
 
     @PostMapping("/logout")
-    public ResponseEntity logout(HttpServletResponse response) {
-        expiredCookie(response, "memberId");
+    public ResponseEntity logout(HttpServletResponse response, HttpServletRequest request) {
+        sessionManager.expire(request);
+        expiredCookie(response, "memberId");//세션 만료 추가
         return new ResponseEntity("로그아웃", HttpStatus.OK);
     }
 
