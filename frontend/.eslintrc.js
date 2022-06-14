d055677ac60ace5332fe89089fa4d92d06a3bfb7
Younggun Park <ygpark0922@gmail.com>
2022-06-14 23:29:52 +0900

    chore: fix module not found error typescript version to 4.4.4

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,6 +1,31 @@
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
-     env: { browser: true, es6: true, node: true },
-      extends: ['eslint:recommended', 'airbnb', 'plugin:prettier/recommended'],
-       rules: { 'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+  env: {
+    browser: true,
+    commonjs: true,
+    amd: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    '@jadru/eslint-config',
+    'eslint:recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+    'react-app',
+    'react-app/jest',
+    'plugin:import/typescript',],
+  rules: {
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        "paths": ["src"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      'typescript': {},
     },
+  },
 };
